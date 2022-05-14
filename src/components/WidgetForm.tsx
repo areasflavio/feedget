b02a948b7af29fd299cfe9c884659df58e799eca
Flávio Arêas <vflavio9@gmail.com>
2022-05-14 20:08:14 -0300

    feat: create WidgetForm header and footer

---
+++ src/components/WidgetForm.tsx
@@ -0,0 +1,26 @@
+import { CloseButton } from './CloseButton';
+
+export function WidgetForm() {
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+
+        <CloseButton />
+      </header>
+
+      <p>WidgetForm</p>
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ por{' '}
+        <a
+          className="underline underline-offset-2"
+          href="http://github.com/areasflavio"
+          target="_blank"
+        >
+          Flávio Arêas
+        </a>
+      </footer>
+    </div>
+  );
+}
